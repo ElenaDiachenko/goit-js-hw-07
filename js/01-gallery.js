eac39375da0dc26808c2d535e45560e535a5fc02
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-05-30 23:28:13 +0300

    Update 01-gallery.js
    create el

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const containerRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const makeItemMarkup = item => {
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src= ${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>
+  `
+}
+const makeAllItemMarkup = galleryItems.map(makeItemMarkup)
+  .join('\n');
+
+containerRef.insertAdjacentHTML('beforeend', makeAllItemMarkup)
+
+console.log(makeItemMarkup);
+
+
