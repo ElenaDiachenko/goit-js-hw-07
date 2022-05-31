ceda31930e39e7b710d88df1c2d86bef25e741dd
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-01 02:23:55 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+
 const containerRef = document.querySelector('.gallery');
+
+containerRef.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    const html = `
+     <img src = "${e.target.dataset.source}" 
+    />
+	`
+
+    const instance = basicLightbox.create(html, {
+        onShow: (instance) => console.log('onShow', instance),
+        onClose: (instance) => console.log('onClose', instance)
+    })
+
+    instance.show((instance) => console.log('finished show()', instance))
+}
 
 const makeItemMarkup = item => {
   return `
@@ -21,6 +43,47 @@ const makeAllItemMarkup = galleryItems.map(makeItemMarkup)
 
 containerRef.insertAdjacentHTML('beforeend', makeAllItemMarkup)
 
-console.log(makeItemMarkup);
+
+
+
+// const containerRef = document.querySelector('.gallery');
+
+// containerRef.addEventListener('click', onOpenModal);
+
+// function onOpenModal(e) {
+//     e.preventDefault();
+//     if (e.target.nodeName !== 'IMG') {
+//          return
+//      }
+//     const instance = basicLightbox.create(`
+// 	 <img src = "${e.target.dataset.source}" 
+//     />
+// `)
+//     instance.show()
+    
+    
+// }
+
+// const makeItemMarkup = item => {
+//   return `
+//   <div class="gallery__item">
+//   <a class="gallery__link" href="${item.original}">
+//     <img
+//       class="gallery__image"
+//       src= ${item.preview}
+//       data-source=${item.original}
+//       alt=${item.description}
+//     />
+//   </a>
+// </div>
+//   `
+// }
+// const makeAllItemMarkup = galleryItems.map(makeItemMarkup)
+//   .join('\n');
+
+// containerRef.insertAdjacentHTML('beforeend', makeAllItemMarkup)
+
+
+
 
 
