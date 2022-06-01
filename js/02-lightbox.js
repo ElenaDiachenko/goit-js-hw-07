bb6785aa039a740ee29979adcfd872f0d0aab2b0
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-01 03:22:17 +0300

    add base

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const containerRef = document.querySelector('.gallery');
+
+// containerRef.addEventListener('click', onOpenModal);
+
+
+const makeItemMarkup = item => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="Image description" />
+</a>
+  `
+}
+const makeAllItemMarkup = galleryItems.map(makeItemMarkup)
+  .join('\n');
+
+containerRef.insertAdjacentHTML('beforeend', makeAllItemMarkup)
+
