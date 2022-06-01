6e91376563340316c563e57fbd963db02ca1d031
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-01 03:08:50 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,21 +7,25 @@ const containerRef = document.querySelector('.gallery');
 containerRef.addEventListener('click', onOpenModal);
 
 function onOpenModal(e) {
-    e.preventDefault();
+  e.preventDefault();
+  window.addEventListener('keydown', onEscCloseModal);
+
     if (e.target.nodeName !== 'IMG') {
         return
     }
-    const html = `
-     <img src = "${e.target.dataset.source}" 
-    />
-	`
-
-    const instance = basicLightbox.create(html, {
-        onShow: (instance) => console.log('onShow', instance),
-        onClose: (instance) => console.log('onClose', instance)
-    })
-
-    instance.show((instance) => console.log('finished show()', instance))
+       const instance = basicLightbox.create(`
+	 <img src = "${e.target.dataset.source}" 
+     />
+ `)
+  instance.show();
+  
+  function onEscCloseModal(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onEscCloseModal)
+    }
+  }
+    
 }
 
 const makeItemMarkup = item => {
@@ -42,48 +46,4 @@ const makeAllItemMarkup = galleryItems.map(makeItemMarkup)
   .join('\n');
 
 containerRef.insertAdjacentHTML('beforeend', makeAllItemMarkup)
-
-
-
-
-// const containerRef = document.querySelector('.gallery');
-
-// containerRef.addEventListener('click', onOpenModal);
-
-// function onOpenModal(e) {
-//     e.preventDefault();
-//     if (e.target.nodeName !== 'IMG') {
-//          return
-//      }
-//     const instance = basicLightbox.create(`
-// 	 <img src = "${e.target.dataset.source}" 
-//     />
-// `)
-//     instance.show()
-    
-    
-// }
-
-// const makeItemMarkup = item => {
-//   return `
-//   <div class="gallery__item">
-//   <a class="gallery__link" href="${item.original}">
-//     <img
-//       class="gallery__image"
-//       src= ${item.preview}
-//       data-source=${item.original}
-//       alt=${item.description}
-//     />
-//   </a>
-// </div>
-//   `
-// }
-// const makeAllItemMarkup = galleryItems.map(makeItemMarkup)
-//   .join('\n');
-
-// containerRef.insertAdjacentHTML('beforeend', makeAllItemMarkup)
-
-
-
-
 
