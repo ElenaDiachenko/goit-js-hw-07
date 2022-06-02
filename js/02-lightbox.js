248fa2f5e299d4d8a28cb79b393ec200236fe84e
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-03 01:05:28 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,16 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 const containerRef = document.querySelector('.gallery');
 
-// containerRef.addEventListener('click', onOpenModal);
+containerRef.addEventListener('click', onOpenModal);
 
+function onOpenModal(e) {
+  e.preventDefault();
+  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+  
+}
 
 const makeItemMarkup = item => {
     return `
     <a class="gallery__item" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}" alt="Image description" />
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
 </a>
   `
 }
